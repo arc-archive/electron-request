3ff010607516166046587611353c13d73d488b6f
Paweł Psztyć <jarrodek@gmail.com>
2019-10-24 08:26:53 -0700

    chore: removing unused file

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const {HostRulesEval} = require('../');
-describe("Test of tests", function() {
-  it("Test if Blob can be directly converted to Buffer", () => {
-    const blob = new Blob(["abc"], { type: "text/plain" });
-    console.log(blob);
-    const buff = Buffer.from(blob);
-    console.log(buff);
-  });
-});
