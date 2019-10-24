0984374d7b5e9e441ef7fad82dda748ba6f95296
Paweł Psztyć <jarrodek@gmail.com>
2019-10-24 08:27:16 -0700

    chore: updating eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
-    "extends": "google",
-    "env": {
-      "browser": true
-    },
-    "rules": {
-      "max-len": ["error", { "code": 120 }]
-    }
+  "extends": "google",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 8
+  },
+  "env": {
+    "browser": true,
+    "mocha": true,
+    "node": true,
+    "es6": true
+  },
+  "rules": {
+    "max-len": ["error", {
+      "code": 120
+    }],
+    "no-undef": "error",
+    "require-jsdoc": 0,
+    "comma-dangle": 0,
+    'linebreak-style': 0
+  }
 };
